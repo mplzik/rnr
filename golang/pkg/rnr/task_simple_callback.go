9bb1c007643c31ec9e9a087f86fba8672b77a134
Milan Plzik <milan.plzik@grafana.com>
2022-02-07 23:18:54 +0100

    Handle errors for non-final cases.
    If an error is reported despite of return boolean being false (continue
    repeating), use that error as a state message.
    
    Signed-off-by: Milan Plzik <milan.plzik@grafana.com>

--- golang/pkg/rnr/task_simple_callback.go
+++ golang/pkg/rnr/task_simple_callback.go
@@ -38,11 +38,17 @@ func (ct *SimpleCallbackTask) Poll() {
 	ctx, _ := context.WithTimeout(context.Background(), 1*time.Second)
 	ret, err := ct.callback(ct, ctx)
 
-	if err != nil {
-		ct.pb.State = pb.TaskState_FAILED
-		ct.pb.Message = err.Error()
-	} else if ret {
-		ct.pb.State = pb.TaskState_SUCCESS
+	if ret {
+		if err == nil {
+			ct.pb.State = pb.TaskState_SUCCESS
+		} else {
+			ct.pb.State = pb.TaskState_FAILED
+			ct.pb.Message = err.Error()
+		}
+	} else {
+		if err != nil {
+			ct.pb.Message = err.Error()
+		}
 	}
 }
 
