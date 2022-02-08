0c42a32e2d98720906fdc571749211af50d4bbfb
Leandro López (inkel) <inkel.ar@gmail.com>
2022-02-08 16:24:29 -0300

    Add tests for taskSchedState
    This is useful for knowing the change I'll make later won't break anything.
    
    Signed-off-by: Leandro López (inkel) <inkel.ar@gmail.com>

---
+++ golang/pkg/rnr/tasks_test.go
@@ -0,0 +1,26 @@
+package rnr
+
+import (
+	"testing"
+
+	"github.com/mplzik/rnr/golang/pkg/pb"
+)
+
+func TestTaskSchedState(t *testing.T) {
+	run := func(name string, exp int, states ...pb.TaskState) {
+		t.Run(name, func(t *testing.T) {
+			for _, s := range states {
+				got := taskSchedState(&pb.Task{State: s})
+				if exp != got {
+					t.Errorf("expecting task %v to map to %v, got %v", s, exp, got)
+				}
+			}
+		})
+	}
+
+	run("DONE", DONE, pb.TaskState_FAILED, pb.TaskState_SKIPPED, pb.TaskState_SUCCESS)
+
+	run("PENDING", PENDING, pb.TaskState_PENDING)
+
+	run("RUNNING", RUNNING, pb.TaskState_ACTION_NEEDED, pb.TaskState_RUNNING)
+}
