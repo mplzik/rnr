f1adf4247ad32682cfe8869750f83395fecd71a6
Leandro López (inkel) <inkel.ar@gmail.com>
2022-03-07 14:57:17 -0300

    Add test for CallbackTask.Poll when callback return errors
    Signed-off-by: Leandro López (inkel) <inkel.ar@gmail.com>

--- golang/pkg/rnr/task_simple_callback_test.go
+++ golang/pkg/rnr/task_simple_callback_test.go
@@ -2,6 +2,7 @@ package rnr
 
 import (
 	"context"
+	"fmt"
 	"testing"
 
 	"github.com/mplzik/rnr/golang/pkg/pb"
@@ -41,6 +42,37 @@ func TestCallbackTask_Poll(t *testing.T) {
 	if callsCount != 2 {
 		t.Errorf("expecting callback to be invoked twice, got %d invokations", callsCount)
 	}
+
+	t.Run("callback returns error", func(t *testing.T) {
+		var done bool
+		fn := func(ctx context.Context, ct *CallbackTask) (bool, error) {
+			return done, fmt.Errorf("done %t", done)
+		}
+		ct := NewCallbackTask("failing callback test", fn)
+
+		ct.Poll()
+
+		pbt := ct.Proto(nil)
+		if s := pbt.State; s != pb.TaskState_RUNNING {
+			t.Errorf("shouldn't update task state: %v", s)
+		}
+		if exp := "done false"; pbt.Message != exp {
+			t.Errorf("expectiing message to be %q, got %q", exp, pbt.Message)
+		}
+
+		ct.SetState(pb.TaskState_UNKNOWN)
+		done = true
+
+		ct.Poll()
+
+		pbt = ct.Proto(nil)
+		if s := pbt.State; s != pb.TaskState_FAILED {
+			t.Errorf("task state should be %v, got %v", pb.TaskState_FAILED, s)
+		}
+		if exp := "done true"; pbt.Message != exp {
+			t.Errorf("expectiing message to be %q, got %q", exp, pbt.Message)
+		}
+	})
 }
 
 func TestCallbackTask_GetChild(t *testing.T) {
