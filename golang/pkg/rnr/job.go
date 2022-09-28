1f52038e8d6900c5041a8b6d42e5cf21aaaedd86
Leandro López (inkel) <inkel.ar@gmail.com>
2022-09-28 15:57:47 -0300

    Make Job context aware
    Here we're changing the Job API to make it context aware while also
    giving the calling user the possibility of gracefully stopping the go
    routine that manages polling the job and its tasks.
    
    The biggest change is in the Job.Start method that now accepts a
    context.Context as its main parameter, and returns a signal function
    for the caller to execute when manually stopping the job.
    
    With these changes now managing jobs become:
    
    ```go
    job := rnr.NewJob(task)
    stop := job.Start(context.Background())
    
    // do something
    
    // effectively stop the polling loop
    stop()
    
    // check if there was an error
    if err := job.Err(); err != nil {
        log.Fatal("job error:", err)
    }
    ```
    
    Manually calling stop doesn't seem like the most interesting option,
    however, by making it context aware we can also control the polling
    loop. In the following example, the job can be cancelled by the user
    pressing `^C` or after 10 seconds, whatever happens first:
    
    ```go
    ctx, stopNotify := signal.NotifyContext(context.Background(), os.Interrupt)
    defer stopNotify()
    ctx, stopTimeout := context.WithTimeout(ctx, 10 * time.Second)
    defer stopTimeout()
    
    job := rnr.NewJob(task)
    job.Start(ctx)
    
    // Wait for job to complete
    
    log.Fatalf("Job finished with: %v", job.Err())
    ```
    
    Signed-off-by: Leandro López (inkel) <inkel.ar@gmail.com>

--- golang/pkg/rnr/job.go
+++ golang/pkg/rnr/job.go
@@ -1,6 +1,7 @@
 package rnr
 
 import (
+	"context"
 	"fmt"
 	"log"
 	"sort"
@@ -12,40 +13,26 @@ import (
 	proto "google.golang.org/protobuf/proto"
 )
 
+// Job polling interval
+var pollInterval = 5 * time.Second
+
 type Job struct {
 	pbMutex  sync.Mutex
 	job      pb.Job
 	root     Task
-	stop     chan struct{}
 	oldProto *pb.Task
+	err      error
 }
 
 func NewJob(root Task) *Job {
-	ret := &Job{
+	return &Job{
 		job: pb.Job{
 			Version: 1,
 			Uuid:    "1235abcdef",
 			Root:    nil,
 		},
 		root: root,
-		stop: make(chan struct{}),
 	}
-
-	go func() {
-		ticker := time.NewTicker(5 * time.Second)
-		for exit := false; !exit; {
-			select {
-			case _, ok := <-ret.stop:
-				if !ok {
-					exit = true
-				}
-			case <-ticker.C:
-				ret.Poll()
-			}
-		}
-	}()
-
-	return ret
 }
 
 func (j *Job) Proto(updater func(*pb.Job)) *pb.Job {
@@ -128,7 +115,7 @@ func taskDiff(path []string, old *pb.Task, new *pb.Task) []string {
 	return ret
 }
 
-func (j *Job) Poll() {
+func (j *Job) Poll(ctx context.Context) {
 	j.root.Poll()
 
 	newProto := j.root.Proto(nil)
@@ -164,7 +151,35 @@ func (j *Job) TaskRequest(r *pb.TaskRequest) error {
 	return nil
 }
 
+// Err returns whatever error might have happened after Start.
+func (j *Job) Err() error { return j.err }
+
 // Start is a shortcut for setting the root task to "running" state.
-func (j *Job) Start() {
+func (j *Job) Start(ctx context.Context) func() {
 	j.root.SetState(pb.TaskState_RUNNING)
+
+	stopCh := make(chan struct{})
+	stopFn := func() { close(stopCh) }
+
+	go func() {
+		ticker := time.NewTicker(pollInterval)
+
+		for {
+			select {
+			case <-stopCh:
+				// job was stopped by calling stopFn
+				return
+
+			case <-ctx.Done():
+				// job stopp due to context being done (e.g. cancelled or timed out)
+				j.err = ctx.Err()
+				return
+
+			case <-ticker.C:
+				j.Poll(ctx)
+			}
+		}
+	}()
+
+	return stopFn
 }
