6c1fa2ca553ba0d2ff30fd32436bf5d7f64f6441
Leandro López (inkel) <inkel.ar@gmail.com>
2022-09-29 08:53:45 -0300

    Add Job tests
    This isn't by any means exhaustive, but checks most of what can be
    done with the current API.
    
    Signed-off-by: Leandro López (inkel) <inkel.ar@gmail.com>

---
+++ golang/pkg/rnr/job_test.go
@@ -0,0 +1,67 @@
+package rnr_test
+
+import (
+	"context"
+	"errors"
+	"testing"
+
+	"github.com/mplzik/rnr/golang/pkg/pb"
+	"github.com/mplzik/rnr/golang/pkg/rnr"
+)
+
+var _ rnr.Task = &task{}
+
+type task struct {
+	pollFn func()
+}
+
+func (t *task) Poll()                         { t.pollFn() }
+func (t *task) SetState(pb.TaskState)         {}
+func (t *task) Proto(func(*pb.Task)) *pb.Task { return nil }
+func (*task) GetChild(string) rnr.Task        { return nil }
+
+func TestJob(t *testing.T) {
+	var pollCount int
+
+	task := &task{
+		pollFn: func() {
+			pollCount++
+			t.Logf("poll %02d", pollCount)
+		},
+	}
+
+	j := rnr.NewJob(task)
+
+	ctx := context.Background()
+
+	var err error
+
+	if err = j.Start(ctx); err != nil {
+		t.Fatalf("unexpected error: %v", err)
+	}
+
+	if err = j.Start(ctx); !errors.Is(err, rnr.ErrJobAlreadyStarted) {
+		t.Fatalf("expecting ErrJobAlreadyStarted, got %v", err)
+	}
+
+	var stopErr error
+
+	go func() {
+		for {
+			if pollCount == 5 {
+				stopErr = j.Stop()
+				break
+			}
+		}
+	}()
+
+	<-j.Wait()
+
+	if stopErr != nil {
+		t.Fatalf("unexpected error: %v", stopErr)
+	}
+
+	if err := j.Stop(); !errors.Is(err, rnr.ErrJobNotRunning) {
+		t.Fatalf("expecting rnr.ErrJobNotRunning, got %v", err)
+	}
+}
